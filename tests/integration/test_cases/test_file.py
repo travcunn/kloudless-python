23c62a8cdc4dbace64b522f8fa4b3e531c58e64c
Travis Cunningham <tcunningham@smartfile.com>
2017-03-01 15:52:35 -0500

    fix tests.

--- tests/integration/test_cases/test_file.py
+++ tests/integration/test_cases/test_file.py
@@ -4,91 +4,110 @@ import random
 import json
 import time
 
-import utils
-import sdk
+import pytest
 
-class File(unittest.TestCase):
+from . import utils
+import kloudless
 
-    # need to perform CRUD on existing file
-    def setUp(self):
-        self.folder = utils.create_or_get_test_folder(self.account)
-        self.file = utils.create_test_file(self.account)
 
-    # CREATE
-    def test_create_file(self):
-        self.assertEqual(self.file.account, self.account.id)
-        new_file = utils.create_test_file(self.account, file_data='test data1',
+@pytest.fixture(params=[acc for acc in utils.accounts])
+def account(request):
+    return request.param
+
+
+class TestFile:
+
+    def create_test_files(self, acc):
+        self.folder = utils.create_or_get_test_folder(acc)
+        self.file = utils.create_test_file(acc)
+
+    def test_create_file(self, account):
+        # CREATE
+        self.create_test_files(account)
+
+        assert self.file.account == account.id
+        new_file = utils.create_test_file(account, file_data='test data1',
                                           overwrite=False)
-        self.assertNotEqual(self.file.name, new_file.name)
-        new_file = utils.create_test_file(self.account, file_data='test data2',
+        assert self.file.name != new_file.name
+        new_file = utils.create_test_file(account, file_data='test data2',
                                           file_name=self.file.name, overwrite=True)
-        self.assertEqual(self.file.name, new_file.name)
+        assert self.file.name == new_file.name
 
-    # Read
-    def test_read_file(self):
-        read_file = self.account.files.retrieve(self.file.id)
-        self.assertTrue(hasattr(read_file, 'id'))
-        self.assertTrue(hasattr(read_file, 'name'))
-        self.assertTrue(hasattr(read_file, 'size'))
-        self.assertTrue(hasattr(read_file, 'created'))
-        self.assertTrue(hasattr(read_file, 'modified'))
-        self.assertEqual(read_file.type, 'file')
-        self.assertTrue(hasattr(read_file, 'account'))
-        self.assertTrue(hasattr(read_file, 'parent'))
-        self.assertTrue(hasattr(read_file, 'ancestors'))
-        self.assertTrue(hasattr(read_file, 'path'))
-        self.assertTrue(hasattr(read_file, 'mime_type'))
-        self.assertTrue(hasattr(read_file, 'downloadable'))
+    def test_read_file(self, account):
+        # Read
+        self.create_test_files(account)
+        read_file = account.files.retrieve(self.file.id)
+        assert hasattr(read_file, 'id')
+        assert hasattr(read_file, 'name')
+        assert hasattr(read_file, 'size')
+        assert hasattr(read_file, 'created')
+        assert hasattr(read_file, 'modified')
+        assert read_file.type == 'file'
+        assert hasattr(read_file, 'account')
+        assert hasattr(read_file, 'parent')
+        assert hasattr(read_file, 'ancestors')
+        assert hasattr(read_file, 'path')
+        assert hasattr(read_file, 'mime_type')
+        assert hasattr(read_file, 'downloadable')
 
-    # Rename, Move and Copy
-    def test_rename_file(self):
+    def test_rename_file(self, account):
+        # Rename, Move and Copy
+        self.create_test_files(account)
         new_name = 'renamed %s' % self.file.name
         self.file.name = new_name
-        self.assertTrue(self.file.save())
-        self.assertEqual(self.file.name, new_name)
-        self.assertEqual(self.file.account, self.account.id)
+        assert self.file.save()
+        assert self.file.name == new_name
+        assert self.file.account == account.id
 
-    def test_move_file(self):
+    def test_move_file(self, account):
+        self.create_test_files(account)
         new_name = 'moved %s' % self.file.name
-        folder1 = self.account.folders.create(
+        folder1 = account.folders.create(
             data={'parent_id': self.folder.id,
                   'name': 'folder %s' % random.randint(0, 10e10)})
-        self.assertTrue(utils.is_folder_present(folder1.name, self.folder))
+        assert utils.is_folder_present(folder1.name, self.folder)
         self.file.parent_id = folder1.id
         self.file.name = new_name
-        self.assertTrue(self.file.name, new_name)
-        self.assertTrue(self.file.save())
-        self.assertFalse(utils.is_file_present(self.file.name, self.folder))
-        self.assertTrue(utils.is_file_present(self.file.name, folder1))
+        assert self.file.name == new_name
+        assert self.file.save()
+        assert not utils.is_file_present(self.file.name, self.folder)
+        assert utils.is_file_present(self.file.name, folder1)
 
-    def test_copy_file(self):
+    def test_copy_file(self, account):
+        self.create_test_files(account)
         new_name = 'copied %s' % self.file.name
-        folder1 = self.account.folders.create(
+        folder1 = account.folders.create(
             data={'parent_id': self.folder.id,
                   'name': 'folder %s' % random.randint(0, 10e10)})
-        self.assertTrue(utils.is_folder_present(folder1.name, self.folder))
+        assert utils.is_folder_present(folder1.name, self.folder)
         new_file = self.file.copy_file(parent_id=folder1.id, name=new_name)
-        self.assertTrue(new_file)
-        self.assertTrue(new_file.name, new_name)
-        self.assertTrue(utils.is_file_present(self.file.name, self.folder))
-        self.assertTrue(utils.is_file_present(new_file.name, folder1))
+        assert new_file
+        assert new_file.name == new_name
+        assert utils.is_file_present(self.file.name, self.folder)
+        assert utils.is_file_present(new_file.name, folder1)
 
-    # Update [update contents]
-    def test_put_file(self):
+    def test_put_file(self, account):
+        # Update [update contents]
+        self.create_test_files(account)
         self.file.update(file_data='hello there')
-        self.assertTrue(self.file.contents().text == 'hello there')
+        assert self.file.contents().text == 'hello there'
 
-    # Delete
-    def test_delete_file(self):
+    def test_delete_file(self, account):
+        # Delete
+        self.create_test_files(account)
         try:
             self.file.delete(permanent=True)
-            read_file = self.account.files.retrieve(self.file.id)
-        except sdk.exceptions.KloudlessException, e:
+            read_file = account.files.retrieve(self.file.id)
+        except kloudless.exceptions.KloudlessException as e:
             error_data = json.loads(str(e).split('Error data: ')[1])
-            self.assertEqual(error_data['status_code'], 404)
+            assert error_data['status_code'] == 404
 
-    @utils.allow(services=['box', 'egnyte', 'gdrive'])
-    def test_properties(self):
+    def test_properties(self, account):
+
+        if account.service not in ['box', 'egnyte', 'gdrive']:
+            return
+
+        self.create_test_files(account)
         def parse(properties):
             result = {}
             for prop in properties['properties']:
@@ -110,9 +129,9 @@ class File(unittest.TestCase):
         ])
 
         properties = parse(properties)
-        self.assertEqual(len(properties), 2)
-        self.assertEqual(properties['key1'], 'value1')
-        self.assertEqual(properties['key2'], 'value2')
+        assert len(properties) == 2
+        assert properties['key1'] == 'value1'
+        assert properties['key2'] == 'value2'
 
         time.sleep(0.5)
         self.file.properties.update(data=[
@@ -133,30 +152,26 @@ class File(unittest.TestCase):
         # Test GET
         time.sleep(0.5)
         properties = parse(self.file.properties.all())
-        self.assertEqual(len(properties), 2)
-        self.assertEqual(properties['key2'], 'hello')
-        self.assertEqual(properties['key3'], 'value3')
+        assert len(properties) == 2
+        assert properties['key2'] == 'hello'
+        assert properties['key3'] == 'value3'
 
         # Test DELETE
         self.file.properties.delete_all()
         time.sleep(0.5)
         properties = parse(self.file.properties.all())
-        self.assertEqual(len(properties), 0)
+        assert len(properties) == 0
 
-    def test_upload_url(self):
-        contents = self.account.folders().contents()
+    def test_upload_url(self, account):
+        if account.service in ['onedrive', 'onedrivebiz', 'sharepoint']:
+            return
+        self.create_test_files(account)
+        contents = account.folders().contents()
         name = 'test_file_name'
         if contents:
             obj = contents[0]
             data = {}
             data['parent_id'] = obj.id
             data['name'] = name
-            response = self.account.files.upload_url(data=data)
-            self.assertTrue('url' in response)
-
-def test_cases():
-    return [utils.create_test_case(acc, File) for acc in utils.accounts]
-
-if __name__ == '__main__':
-    suite = utils.create_suite(test_cases())
-    unittest.TextTestRunner(verbosity=2).run(suite)
+            response = account.files.upload_url(data=data)
+            assert 'url' in response
